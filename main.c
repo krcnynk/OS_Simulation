770df0117172619cabe4e53490b41312a03ed008
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2020-11-14 10:28:00 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "list.h"
 #include "Simulation.h"
 #define MAX_READY_QUEUE 3
+#define MAX_BLOCK_QUEUE 2
 
 bool compare(void* pItem,void* pCompArg)
 {
@@ -10,7 +11,7 @@ bool compare(void* pItem,void* pCompArg)
     return 0;
 }
 
-void List_free(List* pList, FREE_FN pItemFreeFn);
+//void List_free(List* pList, FREE_FN pItemFreeFn);
 
 void freeProcMsg(void* pItem)
 {
@@ -37,7 +38,8 @@ void closeUP(List** readyQ, List** srQ,PCB* runnin)
     free(runnin);
 }
 
-PCB* fetchProcess(List* rQ)
+// Fetches a ready process and removes from the queue
+PCB* fetchProcessFromReadyQueueRemove(List** rQ)
 {
     for(int i = 0;i < MAX_READY_QUEUE;++i)
     {
@@ -50,42 +52,97 @@ PCB* fetchProcess(List* rQ)
     return NULL;
 }
 
-PCB* searchProcess(List* rQ,int PID)
+// PCB* fetchBlockedProcess(List** srQ)
+// {
+//     for(int i = 0;i < MAX_BLOCK_QUEUE;++i)
+//     {
+//         if(List_count(srQ[i]) != 0)
+//         {
+//             return List_trim(srQ[i]);
+//         }
+//     }
+
+//     return NULL;
+// }
+
+// Searches process in all queues
+PCB* searchProcessIfFoundRemove(List** Q,int qSize, int PID)
 {
-    PCB* process = NULL;
-    for(int i = 0; i < MAX_READY_QUEUE; ++i)
+    for(int i = 0; i < qSize; ++i)
     {   
-        // bool (*functionPtr)(void*,void*);
         PCB temp;
-        // functionPtr = &compare;
         temp.PID = input - '0';
-        process = List_search(rQ[i],&compare,&temp);
+        process = List_search(Q[i],&compare,&temp);
+        if(process != NULL)
+        {
+            return List_remove(Q[i]);
+        }
     }
 
-    return process;
+    return NULL;
 }
 
+// PCB* removeReadyProcess(List** rQ,int PID)
+// {
+//     PCB* process = NULL;
+//     for(int i = 0; i < MAX_READY_QUEUE; ++i)
+//     {   
+//         PCB temp;
+//         temp.PID = input - '0';
+//         process = List_search(rQ[i],&compare,&temp);
+//         if(process != NULL)
+//         {
+//             PCB* removed = List_remove(rQ[i]);
+//             free(removed);
+//             break;
+//         }
+//     }
+//     return process;
+// }
+
+int countProcess(List** rq,List**srq)
+{
+    int count = 0;
+    if(rq != NULL)
+    {
+        for(int i = 0; i < MAX_READY_QUEUE; ++i)
+            count = count + List_count(rq[i]);
+    }
+    if(srq != NULL)
+    {
+        for(int i = 0; i < MAX_BLOCK_QUEUE; ++i)
+            count = count + List_count(srq[i])
+    }
+    return count;
+}
 int main()
 {
     List* readyQueueHigh = List_create();
     List* readyQueueNormal = List_create();
     List* readyQueueLow = List_create();
-    List** ReadyQueues[3];
     List* processSendWaitQueue = List_create();
     List* processReceiveWaitQueue = List_create();
-    List** SendReceiveWaitQueue[2];
+    List* ReadyQueues[3];
+    List* SendReceiveWaitQueue[2];
+    List* allQueues[5];
     int globalPID = 1;
     char input;
     char msg[40];
-    int processCount = 1;
 
     ReadyQueues[0] = readyQueueHigh;
     ReadyQueues[1] = readyQueueNormal;
     ReadyQueues[2] = readyQueueLow;
     SendReceiveWaitQueue[0] = processSendWaitQueue;
     SendReceiveWaitQueue[1] = processReceiveWaitQueue;
+    allQueues[0] = ReadyQueues[0];
+    allQueues[1] = ReadyQueues[1];
+    allQueues[2] = ReadyQueues[2];
+    allQueues[3] = SendReceiveWaitQueue[0];
+    allQueues[4] = SendReceiveWaitQueue[1];
 
-    PCB* running;
+
+    PCB* running = NULL;
+    List* init_MSG = List_create();
 
     while(1)
     {
@@ -107,7 +164,6 @@ int main()
 
                     if(List_prepend(ReadyQueues[input],pcbP) == 0)
                     {
-                        ++processCount;
                         ++globalPID;
                         printf("Process PID:%d successfully created.",PID);
                     }
@@ -125,20 +181,14 @@ int main()
 
             case 'F':
             {
-                if(processCount == 1)
-                {
-                    printf("Attempting to fork init is illegal.\n");
-                    break;
-                }
                 PCB* forkedP = malloc(sizeof(PCB));
                 forkedP->PID = globalPID;
                 forkedP->priority = running->priority;
                 forkedP->processState = READY;
                 forkedP->proc_message = List_create();
 
-                if(List_prepend(ReadyQueues[running->priority],forkedP) == 0)
+                if(List_prepend(ReadyQueues[forkedP->priority],forkedP) == 0)
                 {
-                    ++processCount;
                     ++globalPID;
                     printf("Process PID:%d successfully forked.",running->PID);
                 }
@@ -153,30 +203,51 @@ int main()
                 printf("Enter PID of the process to kill.\n");
                 scanf("%c",input);
 
-                if(processCount == 1 && input == 0)
+                if(countProcess(ReadyQueues,SendReceiveWaitQueue) == 0 && input == 0)
                 {
                     printf("Exiting the program"\n);
                     closeUP(ReadyQueues,SendReceiveWaitQueue,running);
                     return 0;
                 }
-                else
+                else if (countProcess(ReadyQueues,SendReceiveWaitQueue) > 1 && input == 0)
                 {
-                    PCB* ret = searchProcess(ReadyQueues, input);
-                    if(ret != NULL)
+                    printf("Killing init is illegal while there are other processes are in the system"\n);
+                }
+                else if (running->PID == input)
+                {
+                    free(running);
+                    printf("Killed the running process %d\n");
+                    PCB* fetched = fetchProcessFromReadyQueueRemove(ReadyQueues);
+                    if(fetched == NULL)
                     {
-                        printf("Killing process %d",input);
-                        free(ret);
+                        printf("Init is running now\n");
+                        running = NULL;
                     }
                     else
                     {
-                        printf("Could not find the process: %d",input);
+                        fetched->processState = RUNNING;
+                        running = fetched;
+                        printf("Process %d is running now\n",input);
+                    }
+                }
+                else
+                {
+                    PCB* ret = searchProcessIfFoundRemove(allQueues,MAX_READY_QUEUE + MAX_BLOCK_QUEUE,input);
+                    if(ret != NULL)
+                    {
+                        free(ret);
+                        printf("Killed the process %d",input);
+                    }
+                    else
+                    {
+                        printf("Could not find the process in the system: %d",input);
                     }
                 }
             }
 
             case 'E':
             {
-                if(processCount == 1)
+                if(countProcess(ReadyQueues,SendReceiveWaitQueue) == 0)
                 {
                     printf("Exiting the program"\n);
                     closeUP(ReadyQueues,SendReceiveWaitQueue,running);
@@ -184,29 +255,29 @@ int main()
                 }
                 else
                 {
-                    printf("Removing running process %d",running->PID);
                     free(running);
-                    --processCount;
-
-                    PCB* fetched = fetchProcess(ReadyQueues);
-                    if(fetched != NULL)
+                    printf("Killed the running process %d\n");
+                    PCB* fetched = fetchProcessFromReadyQueueRemove(ReadyQueues);
+                    if(fetched == NULL)
                     {
-                        running = ret;
-                        fetched->processState = RUNNING;
-                        printf("Process %d is running now",running->PID);
+                        printf("Init is running now\n");
+                        running = NULL;
                     }
                     else
                     {
-                        printf("Could not find any process,FATAL ERROR");
+                        fetched->processState = RUNNING;
+                        running = fetched;
+                        printf("Process %d is running now\n",input);
                     }
                 }
 
             }
+
             case 'Q':
             {
-                if(processCount == 1)
+                if(countProcess(ReadyQueues,SendReceiveWaitQueue) == 0)
                 {
-                    printf("Attempting to Quantum init is illegal.\n");
+                    printf("Ready queue is empty,Quantum not applicable\n");
                     break;
                 }
                 else
@@ -214,22 +285,24 @@ int main()
                     running->processState = READY;
                     if(List_prepend(ReadyQueues[running->priority] == 0)
                     {
-                        printf("Running process %d has been placed back into %s queue", running->PID, priorityString[running->priority]);
+                        printf("Running process %d has been placed back into %s priority queue", running->PID, priorityString[running->priority]);
 
-                        PCB* fetched = fetchProcess(ReadyQueues);
-                        if(ret != NULL)
+                        PCB* fetched = fetchProcessFromReadyQueueRemove(ReadyQueues);
+                        if(fetched == NULL)
                         {
-                            running = fetched;
-                            fetched->processState = RUNNING;
-                            printf("Process %d is running now",running->PID);
+                            printf("Init is running now\n");
+                            running = NULL;
                         }
                         else
                         {
-                            printf("Could not find any process,FATAL ERROR");
+                            fetched->processState = RUNNING;
+                            running = fetched;
+                            printf("Process %d is running now\n",input);
                         }
                     }
                 }
             }
+
             case 'S':
             {
                 printf("Enter PID of the process\n");
@@ -237,68 +310,106 @@ int main()
                 printf("Enter and message(max 40 chars)\n");
                 scanf("%s",msg);
 
-                if(processCount == 1)
+                if(countProcess(ReadyQueues,SendReceiveWaitQueue) == 0)
                 {
-                    printf("Attempting Send to init is illegal.\n");
-                    break;
+                    printf("There is no process to send message\n");
                 }
                 else
                 {
-                    PCB* ret = searchProcess(rQ,input);
+                    PCB* ret = searchProcessIfFoundRemove(allQueues,MAX_READY_QUEUE + MAX_BLOCK_QUEUE,input);
                     if(ret == NULL)
                     {
-                        printf("No process with this id lives in this system\n");
+                        printf("No process with this PID lives in this system\n");
                     }
                     else
                     {
-                        List_prepend(ret->proc_message, msg);
-                        running->processState = BLOCKED;
-                        List_prepend(SendReceiveWaitQueue[0],running);
-                        PCB* fetched = fetchProcess(ReadyQueues);
-                        fetched->processState = RUNNING;
-                        running = fetched;
-                    }
-                }
-            }
-            case 'R':
-            {
-                if(processCount == 1)
-                {
-                    printf("Attempting Receive to init is illegal.\n");
-                    break;
-                }
-                else
-                {
-                    if(List_count(running->proc_message) == 0)
-                    {
-                        printf("There are no messages to be received blocking\n");
-                        running->processState = BLOCKED;
-                        List_prepend(SendReceiveWaitQueue[1],running);
-                        PCB* fetched = fetchProcess(ReadyQueues);
-                        fetched->processState = RUNNING;
-                        running = fetched;
-                    }
-                    else
-                    {
-                        char* msg = List_trim(running->proc_message);
-                        if(msg == NULL)
+                        if(List_prepend(ret->proc_message, msg) == -1)
                         {
-                            printf("NULL message\n");
+                            printf("Buffer overflow in Receiving process,message is getting dumped\n");
                         }
                         else
                         {
-                            printf("Received message %s\n",msg);
-                            free(msg);
+                            printf("Message has been sent to process %d\n",input);
+                            running->processState = BLOCKED;
+                            List_prepend(SendReceiveWaitQueue[0],running);
+                            PCB* fetched = fetchProcessFromReadyQueueRemove(ReadyQueues);
+                            if(fetched == NULL)
+                            {
+                                printf("Init is running\n");
+                            }
+                            else
+                            {
+                                fetched->processState = RUNNING;
+                                running = fetched;
+                            }
+                        }
+                    }
+                }
+            }
+
+            case 'R':
+            {
+                if(List_count(running->proc_message) == 0) // RUNNING PROCESS HAS NO MESSAGE
+                {
+                    printf("There are no messages to be received, blocking the running process\n");
+                    running->processState = BLOCKED;
+                    List_prepend(SendReceiveWaitQueue[1],running);
+                    PCB* fetched = fetchProcessFromReadyQueueRemove(ReadyQueues);
+                    if(fetched == NULL)
+                    {
+                        printf("Init is running\n");
+                    }
+                    else
+                    {
+                        fetched->processState = RUNNING;
+                        running = fetched;
+                    }
+                }
+                else // RUNNING PROCESS HAS A MESSAGE
+                {
+                    char* msg = List_trim(running->proc_message);
+                    printf("Received message %s\n",msg);
+                    free(msg);
+                }   
+            }
+
+            case 'Y':
+            {
+                printf("Enter PID of the process\n");
+                scanf("%c",input);
+                printf("Enter and reply message(max 40 chars)\n");
+                scanf("%s",msg);
+
+                PCB* ret = searchProcessIfFoundRemove(processSendWaitQueue,1,input);
+                if(ret == NULL)
+                {
+                    printf("No process with this PID blocked on \"Send\" in this system\n");
+                }
+                else
+                {
+                    ret->processState = READY;
+                    if(List_prepend(ReadyQueues[running->priority] == 0)
+                    {
+                        printf("Running process %d has been placed back into %s priority queue", running->PID, priorityString[running->priority]);
+                        if(List_prepend(ret->proc_message, msg) == -1)
+                        {
+                            printf("Buffer overflow in Sending process,reply is getting dumped\n");
+                        }
+                        else
+                        {
+                            printf("Reply has been sent to process %d\n",input);
                         }
                         
                     }
                 }
             }
-            case 'Y':
-            {
-                
-            }
+            case 'N':
+            case 'P':
+            case 'V':
+            case 'I':
+            case 'T':
         }
+        if(running)
     }
 
 
